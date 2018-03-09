d353e9738f0fdb181e1388df3e50c8cfdb2bd888
sonam079 <s.shreyna079@gmail.com>
2018-03-09 23:09:31 +0530

    splash file added

---
+++ Splash/src/com/example/splash/Splash2Activity.java
@@ -0,0 +1,15 @@
+package com.example.splash;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Splash2Activity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash2);
+	}
+}
